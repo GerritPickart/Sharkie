2809af16aac9ad5399a361c8aaebfa9b2a51deb6
GerritPickart <gerrit.pickart@gmail.com>
2022-12-16 20:54:28 +0100

    added character animation

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,19 +4,30 @@ class MoveableObject {
     img;
     height = 200;
     width = 200;
+    imageCache = {};
 
-// loadImage('img/test.png')
-    loadImage(path){
+    // loadImage('img/test.png')
+    loadImage(path) {
         this.img = new Image();
         this.img.src = path;
 
     }
 
-    moveRight(){
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+       });
+
+
+    }
+
+    moveRight() {
         console.log('moving right');
     }
 
-    moveLeft(){
-        
+    moveLeft() {
+
     }
 }

93062d76632dc14a99761eb18eaa2e7e0075a3cd
Gerrit Pickart <gerrit.pickart@gmail.com>
2023-01-10 22:25:37 +0100

    added animations

--- models/character.class.js
+++ models/character.class.js
@@ -11,8 +11,42 @@ class Character extends MoveableObject {
         'img/1.Sharkie/3.Swim/4.png',
         'img/1.Sharkie/3.Swim/5.png',
         'img/1.Sharkie/3.Swim/6.png',
-
     ];
+
+    IMAGES_SHOOTING = [
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/1.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/2.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/3.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/4.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/5.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/6.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/7.png',
+        'img/1.Sharkie/4.Attack/Bubble trap/op1 (with bubble formation)/8.png',
+    ];
+
+    IMAGES_DEAD =[
+        'img/1.Sharkie/6.dead/1.Poisoned/1.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/2.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/3.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/4.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/5.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/6.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/7.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/8.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/9.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/10.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/11.png',
+        'img/1.Sharkie/6.dead/1.Poisoned/12.png'
+    ];
+
+    IMAGES_HURT =[
+        'img/1.Sharkie/5.Hurt/1.Poisoned/1.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/2.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/3.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/4.png',
+        'img/1.Sharkie/5.Hurt/1.Poisoned/5.png'
+    ];
+
 
     world;
     swimming_audio = new Audio('audio/bubbling-water.wav');
@@ -20,6 +54,9 @@ class Character extends MoveableObject {
     constructor() {
         super().loadImage('img/1.Sharkie/1.IDLE/1.png');
         this.loadImages(this.IMAGES_SWIMMING);
+        this.loadImages(this.IMAGES_SHOOTING);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
         this.animate();
 
     }
@@ -48,11 +85,16 @@ class Character extends MoveableObject {
 
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
 
-                this.playAnimation(this.IMAGES_SWIMMING);
-             
-            }
+            if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+
+            } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_SWIMMING);
+                }
+                else if(this.isHurt()){
+                    this.playAnimation(this.IMAGES_HURT);
+                }
         }, 50);
     }
 
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -8,6 +8,20 @@ class MoveableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 1;
+    energy = 100;
+    lastHit = 0;
+
+
+    applyGravity() {
+        setInterval(() => {
+            this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+
+        }, 1000 / 25);
+    }
+
 
     // loadImage('img/test.png')
     loadImage(path) {
@@ -15,6 +29,51 @@ class MoveableObject {
         this.img.src = path;
 
     }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.height, this.width);
+    }
+
+    drawFrame(ctx) {
+
+        if (this instanceof Character || this instanceof Pufferfish) {
+            ctx.beginPath();
+            ctx.lineWidth = "5";
+            ctx.strokeStyle = "red";
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
+    
+    isColliding (mo) {
+        return this.x + this.width > mo.x &&
+        this.y + this.height > mo.y &&
+        this.x < mo.x &&
+        this.y < mo.y + mo.height            
+    }
+
+
+    hit() {
+        this.energy -= 10;
+        if(this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
+    }
+
+
+    isDead(){
+        return this.energy == 0;
+    }
+
 
     loadImages(arr) {
         arr.forEach((path) => {
@@ -26,7 +85,7 @@ class MoveableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_SWIMMING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
